a7d05910915980a81bd55b59e90728e5bd08acdf
pawanbprofile <pawanb.profile@gmail.com>
2024-12-09 10:02:05 +0530

    code cleanup

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import { initializeApp, applicationDefault } from "firebase-admin/app";
 import { getMessaging } from "firebase-admin/messaging";
 import express, { json } from "express";
+import cors from "cors";
 
 process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  next();
+});
 initializeApp({
   credential: applicationDefault(),
   projectId: "fir-rndemo-524ce",
